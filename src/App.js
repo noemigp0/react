8ab9746eadb08d21ce03dacb2d1cad388ae49441
Noemi Gutierrez <noemigp0@gmail.com>
2022-07-21 21:46:40 -0500

    renderizing input and states

--- src/App.js
+++ src/App.js
@@ -3,31 +3,26 @@ import { useState } from 'react'
 
 function App () {
   const [title, setTitle] = useState("Hola Koders!")
+  const [isLogged, setIsLogged] = useState(false)
   let userData = {}
-  const inputHandler = (event)=> {
-    console.log(event.target.value)
-  }
-
-  const saveHandler = (event) => {
-    event.preventDefault() //Evita el comportamiento por default
-    alert("Datos guardados")
-  }
   
-  return (
-    <div className='App'>
-     <form actions="" className='bg-dark text-white border p-3 w-50 mx-auto rounded'>
-      <div className='form-group'>
-        <label htmlFor="">Nombre</label>
-        <input type="text" className='form-control' onChange={inputHandler}></input>
-      </div>
-      <div className='form-group'>
-        <label htmlFor="">Email</label>
-        <input type="text" className='form-control' onChange={inputHandler}></input>
-      </div>
-      <button  onClick={saveHandler}></button>
-     </form>
-    </div>
-  )
-}
+  const inputHandler = event => {
+    setTitle(event.target.value)
+  }
 
+  const loginHandler = () => {
+    setIsLogged(true)
+  }
+  const logOutHandler = () => {
+    setIsLogged(false)
+  }
+
+  return (
+   <div className="App">
+   { !isLogged && <button className='btn btn-success' onClick={loginHandler}>login</button>}
+   { isLogged && <button className='btn btn-warning'  onClick={logOutHandler}>sing out</button>}
+   </div>
+  )
+
+  }
 export default App
