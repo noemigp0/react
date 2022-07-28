a8d19de1a9970e8f89c3552efd8f770805297beb
Noemi Gutierrez <noemigp0@gmail.com>
2022-07-27 20:01:44 -0500

    Useeffect incomplete

--- src/App.js
+++ src/App.js
@@ -10,90 +10,44 @@ import {
 } from "reactstrap";
 
 function App() {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const toggle = () => setDropdownOpen((prevState) => !prevState);
+ const [kodersList, setKoderList] = useState([{
+  name: 'Israel',
+  generation: 'x'
+ }])
 
-  const [selectedProducts, setSelectedProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState(products);
-  const addProduct = (id) => {
-    console.log(id);
-    const selectedProduct = products.find((product) => product.id === id);
-    console.log(selectedProduct);
-    setSelectedProducts([
-      ...selectedProducts,
-      { ...selectedProduct, purchased: true },
-    ]);
-  };
-  const removeProduct = (id) => {
-    console.log(id);
-    const remainingProducts = selectedProducts.filter(
-      (product) => product.id !== id
-    );
-    setSelectedProducts(remainingProducts);
-  };
+ useEffect(()=> {
+  const getKoders = async () => {
+    let data = await fetch('https://koder19g-ngp-default-rtdb.firebaseio.com/.json')
+    data = await data.json()
+    setKoderList(data)
+  }
+  getKoders()
+ },[])
 
-  const categoriesList = products.reduce((accum, current) => {
-    return !accum.includes(current.category)
-      ? [...accum, current.category]
-      : accum;
-  }, []);
+ return (
+  <div className="App">
+    <div className="row row-cols-1 row-cols-md-2 g-4">
+      {
+        Object.keys(kodersList).map((koderKey, index) => {
+          console.log(koderKey)
+          console.log(kodersList[koderKey])
+        
 
-  const optionSelected = (value) => {
-    let filteredProducts = products.filter((cv, index) => {
-      return cv.category === value;
-    });
-
-    setFilteredProducts(filteredProducts);
-  };
-
-  console.log(categoriesList);
-  return (
-    <div className="App">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 col-md-6">
-            <h1>Catálogo de productos</h1>
-            <div className="d-flex p-5">
-              <UncontrolledDropdown>
-                <DropdownToggle caret>Categoria</DropdownToggle>
-                <DropdownMenu>
-                  {categoriesList.map((item, index) => {
-                    return (
-                      <DropdownItem onClick={() => optionSelected(item)}>
-                        {item}
-                      </DropdownItem>
-                    );
-                  })}
-                </DropdownMenu>
-              </UncontrolledDropdown>
+          return (
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <div className="card-text">Name: </div>
+                <div className="card-text">Generacion: </div>
+              </div>
             </div>
 
-            <ItemsList
-              productsArray={filteredProducts}
-              addProductHandler={addProduct}
-            />
-          </div>
-          <div className="col-12 col-md-6">
-            <h1>Carrito de compras</h1>
-            {!selectedProducts.length ? (
-              <h2>Selecciona algún producto de la lista</h2>
-            ) : (
-              <>
-                <h3>
-                  Total:
-                  {selectedProducts.reduce((acc, curr) => acc + curr.price, 0)}
-                </h3>
-                <ItemsList
-                  productsArray={selectedProducts}
-                  removeProductHandler={removeProduct}
-                />
-              </>
-            )}
-          </div>
-        </div>
-      </div>
+          </div>)
+        })
+      }
     </div>
-  );
+  </div>
+ )
 }
 
 export default App;
